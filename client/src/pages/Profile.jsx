4bd91a92b0e6aeb4ece46496fe800e936c905427
SSivanesan1812 <ssivanesan1812@gmail.com>
2024-03-02 20:50:26 +0530

    Created upload image functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
+import { app } from '../firebase/auth'
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 
 export default function Profile() {
   const {currentuser}=useSelector((state)=>state.user)
-  console.log(currentuser)
+  const imageRef=useRef(null)
+  const [image,setImage]=useState(null)
+  const [imagePerc,setImagePerc]=useState(null)
+  const [imError,SetImError]=useState(null)
+  const [formdata,setFormData]=useState({})
+  console.log(formdata)
+  useEffect(()=>{
+    if(image){
+      uploadFile(image)
+    }
+  },[image])
+  const uploadFile=(image)=>{
+    const storage=getStorage(app);
+    const filename=new Date().getTime() +image.name;
+    const storageRef=ref(storage,filename)
+    SetImError(null)
+    const imageUpload=uploadBytesResumable(storageRef,image)
+    imageUpload.on('state_changed',
+    (snapshot)=>{
+      const percentage=Math.round((snapshot.bytesTransferred/snapshot.totalBytes)*100);
+      console.log(percentage)
+
+      setImagePerc(percentage)
+
+    },
+    (errror)=>{
+      console.log(errror)
+      SetImError(errror)
+    },
+    ()=>{
+      getDownloadURL(imageUpload.snapshot.ref).then((downloadUrl)=>{
+        setFormData({...formdata,avatar:downloadUrl})
+      })
+    }
+    
+    )
+
+  }
   return (
     <div className='max-w-lg mx-auto p-3'>
       <h1 className='text-3xl font-bold text-center p-3'>Profile</h1>
       <form className='flex flex-col gap-3 pt-3'>
-        <img src={currentuser.photo} className='h-24 w-24 rounded-full object-cover self-center'/>
+        <input type='file' hidden  ref={imageRef} onChange={(e)=>setImage(e.target.files[0])} accept='image/*'></input>
+        <img src={ formdata.avatar || currentuser.photo} onClick={()=>{imageRef.current.click()}} 
+        className='h-24 w-24 rounded-full object-cover self-center cursor-pointer'/>
+        {imagePerc  && imagePerc!==100 ? <span className='text-center'>{`Uploaded ${imagePerc} percentage`} </span> : 
+        <span className='text-red-600 text-center'>{ imError && ` upload image less than 2mb`}</span>}
         <input type='text' placeholder='username' id='username' className='rounded-lg p-3 border'></input>
         <input type='text' placeholder='email' id='email' className='rounded-lg p-3 border'></input>
         <input type='text' placeholder='password' id='password' className='rounded-lg p-3 border'></input>
