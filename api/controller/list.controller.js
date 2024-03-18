643c251308bc09afd35ee7093f269ee6d3576b36
SSivanesan1812 <ssivanesan1812@gmail.com>
2024-03-18 20:03:43 +0530

    Create user listing api route

--- api/controller/list.controller.js
+++ api/controller/list.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../model/list.model.js"
+import { errorGenerator } from "../utils/error.js"
 
 
 export const createListing=async (req,res,next)=>{
@@ -11,4 +12,18 @@ export const createListing=async (req,res,next)=>{
         next(error)
     }
 
+}
+
+export const createUserListing=async (req,res,next)=>{
+    try {
+        if(req.user.id===req.params.id){
+            const listing=await Listing.find({userRef:req.params.id})
+            res.status(201).json(listing)
+        }
+        else{
+            return next(errorGenerator(401,"You can only access your own list"))
+        }
+    } catch (error) {
+        next(error)
+    }
 }
--- api/routes/list.route.js
+++ api/routes/list.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { createListing } from "../controller/list.controller.js";
+import { createListing, createUserListing } from "../controller/list.controller.js";
 
 const route=express.Router()
 
 route.post('/createlist',verifyUser,createListing)
+route.get('/userlisting/:id',verifyUser,createUserListing)
 
 export default route;
 
